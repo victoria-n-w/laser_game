b6eca95e45768f9673fac0c38fca25709693bd18
mjw <marekjwesolowski@gmail.com>
2022-09-11 22:47:28 +0200

    Refactor Player Bundle

--- src/player/entity.rs
+++ src/player/entity.rs
@@ -14,13 +14,84 @@ pub struct PlayerBundle {
     #[bundle]
     sprite: SpriteBundle,
     controls: movement::controls::Controls,
+    dynamics: movement::dynamics::Dynamics,
     collisions: collisions::Collideable,
     attacking: attack::Attacking,
     size: common::size::Size,
 }
 
-#[derive(Component)]
+impl PlayerBundle {
+    pub fn new(x: f32, y: f32, rotation: f32, asset_server: &Res<AssetServer>) -> Self {
+        PlayerBundle {
+            player: Player,
+            name: Name::new("PLAYER"),
+            sprite: SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(50.0, 50.0)),
+                    ..default()
+                },
+                texture: asset_server.load("red.png"),
+                transform: Transform {
+                    translation: Vec3::new(x, y, 0.0),
+                    rotation: Quat::from_rotation_z(rotation),
+                    scale: Vec3::new(1.0, 1.0, 1.0),
+                },
+                ..default()
+            },
+            collisions: collisions::Collideable,
+            controls: movement::controls::Controls::default(),
+            attacking: attack::Attacking::new(3_f32, 1_f32),
+            size: common::size::Size { radius: 25.0 },
+            dynamics: movement::dynamics::Dynamics {
+                linear_acceleration: 400.0,
+                idle_breaking: 50.0,
+                max_speed: 300.0,
+                min_spped: -300.0,
+                current_speed: 0.0,
+                turning_speed: 5.0,
+                current_turning_speed: 0.0,
+            },
+        }
+    }
+}
+
+#[derive(Component, Default)]
 pub struct Fire;
+
+#[derive(Bundle)]
+struct FireBundle {
+    #[bundle]
+    sprite: SpriteSheetBundle,
+    animation: animation::Repeating,
+    fire: Fire,
+}
+
+impl FireBundle {
+    pub fn new(
+        fps: i32,
+        asset_server: &Res<AssetServer>,
+        texture_atlases: &mut ResMut<Assets<TextureAtlas>>,
+    ) -> Self {
+        let texture_handle = asset_server.load("fire.png");
+        let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 30, 1);
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+        FireBundle {
+            sprite: SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                transform: Transform {
+                    translation: Vec3::new(-100.0, 0.0, 0.0),
+                    rotation: Quat::from_rotation_z(std::f32::consts::FRAC_PI_2),
+                    scale: Vec3::new(4.0, 4.0, 1.0),
+                },
+                visibility: Visibility { is_visible: false },
+                ..default()
+            },
+            animation: animation::Repeating(Timer::from_seconds(1_f32 / fps as f32, true)),
+            fire: Fire,
+        }
+    }
+}
 
 #[allow(clippy::needless_pass_by_value)] // bevy requires Res to be passed by value
 pub fn spawn_player(
@@ -29,52 +100,11 @@ pub fn spawn_player(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     let player = commands
-        .spawn()
-        .insert(Player)
-        .insert(Name::new("Player"))
-        .insert_bundle(SpriteBundle {
-            sprite: Sprite {
-                custom_size: Some(Vec2::new(50.0, 50.0)),
-                ..default()
-            },
-            texture: asset_server.load("red.png"),
-            ..default()
-        })
-        .insert(movement::controls::Controls::default())
-        .insert(movement::dynamics::Dynamics {
-            linear_acceleration: 400.0,
-            idle_breaking: 50.0,
-            max_speed: 300.0,
-            min_spped: -300.0,
-            current_speed: 0.0,
-            turning_speed: 5.0,
-            current_turning_speed: 0.0,
-        })
-        .insert(collisions::Collideable)
-        .insert(attack::Attacking::new(3_f32, 1_f32))
-        .insert(common::size::Size { radius: 25.0 })
+        .spawn_bundle(PlayerBundle::new(0.0, 0.0, 0.0, &asset_server))
         .id();
 
-    let texture_handle = asset_server.load("fire.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 30, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
     let fire = commands
-        .spawn_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform {
-                translation: Vec3::new(-100.0, 0.0, 0.0),
-                rotation: Quat::from_rotation_z(std::f32::consts::FRAC_PI_2),
-                scale: Vec3::new(4.0, 4.0, 1.0),
-            },
-            visibility: Visibility { is_visible: false },
-            ..default()
-        })
-        .insert(animation::Repeating(Timer::from_seconds(
-            1_f32 / 30_f32,
-            true,
-        )))
-        .insert(Fire)
+        .spawn_bundle(FireBundle::new(30, &asset_server, &mut texture_atlases))
         .id();
 
     commands.entity(player).add_child(fire);
